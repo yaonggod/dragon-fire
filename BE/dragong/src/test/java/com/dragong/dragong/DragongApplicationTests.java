b16a08dd2b976707578b3b784b506cc8a142866c
태준모 <sunjm135@naver.com>
2023-10-16 15:26:32 +0900

    :tada: Start: Initial commit

---
+++ BE/dragong/src/test/java/com/dragong/dragong/DragongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragong.dragong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
