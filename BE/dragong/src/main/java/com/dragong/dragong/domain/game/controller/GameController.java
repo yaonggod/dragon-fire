e2d2848d7162b46717e371be7d6f15d9a4ee8507
gogobattle@naver.com <gogobattle@naver.com>
2023-10-16 18:02:29 +0900

    feat: game 구현중

--- BE/dragong/src/main/java/com/dragong/dragong/domain/game/controller/GameController.java
+++ BE/dragong/src/main/java/com/dragong/dragong/domain/game/controller/GameController.java
@@ -1,2 +1,36 @@
-package com.dragong.dragong.domain.game.controller;public class GameController {
+package com.dragong.dragong.domain.game.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/game")
+public class GameController {
+
+    @MessageMapping("/play")
+    @SendTo("/topic/results")
+    public ResponseEntity<String> playGame(String choice) {
+        // 게임 로직을 구현
+        // choice: 가위, 바위, 보 중 하나의 선택
+
+        // 게임 결과 반환
+        String result = "You win!";
+        return ResponseEntity.ok(result);
+    }
+
+    @RequestMapping(value = "/play", method = RequestMethod.GET)
+    public ResponseEntity<String> playGameREST(String choice) {
+        // 게임 로직을 구현
+        // choice: 가위, 바위, 보 중 하나의 선택
+
+        // 게임 결과 반환
+        String result = "You win!";
+        return ResponseEntity.ok(result);
+    }
 }
--- BE/dragong/src/main/java/com/dragong/dragong/global/config/WebSocketConfig.java
+++ BE/dragong/src/main/java/com/dragong/dragong/global/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.dragong.dragong.global;
+package com.dragong.dragong.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
