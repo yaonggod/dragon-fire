9100eac3bb8cafaa8e5fa2c748d1b0dbd17fc13b
김재석 <jaeseki@gmail.com>
2023-10-23 17:02:20 +0900

    :iphone: Design: 스크린 페이지 기본 레이아웃 구성

--- frontend/lib/screens/main_screen.dart
+++ frontend/lib/screens/main_screen.dart
@@ -10,6 +10,28 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Column(
+            children: [
+              Text(
+                '드래곤 불',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                '천하제일 쎄쎄쎄',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              )
+            ],
+          ),
+        ),
+        backgroundColor: Colors.red[100],
+      ),
+    );
   }
 }
