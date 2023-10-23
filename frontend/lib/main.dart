91b53d64232074b73735a2906ef8b38c0baad8b8
김재석 <jaeseki@gmail.com>
2023-10-23 13:43:51 +0900

    :tada: Start: Initial commit

---
+++ frontend/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DragonG());
+}
+
+class DragonG extends StatelessWidget {
+  const DragonG({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '드래곤 불',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
