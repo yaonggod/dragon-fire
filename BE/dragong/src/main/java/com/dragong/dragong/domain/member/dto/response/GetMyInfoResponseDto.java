22449f1c8780716a35ec21f73797653623c44417
태준모 <sunjm135@naver.com>
2023-11-13 15:28:57 +0900

    :sparkles: Feat: 자기소개 추가
    - 자기소개 수정 로직 추가
    - 자기소개 로그인시 불러오도록 변경
    - 멤버 정보 불러오는 api 추가

--- BE/dragong/src/main/java/com/dragong/dragong/domain/member/dto/response/GetMyInfoResponseDto.java
+++ BE/dragong/src/main/java/com/dragong/dragong/domain/member/dto/response/GetMyInfoResponseDto.java
@@ -3,17 +3,17 @@ package com.dragong.dragong.domain.member.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.checkerframework.checker.units.qual.N;
 
 @Getter
 @NoArgsConstructor
-public class LoginResponseDto {
-
-    private String nickname;
-    private String email;
-    private String introduction;
+public class GetMyInfoResponseDto {
+    String nickname;
+    String email;
+    String introduction;
 
     @Builder
-    public LoginResponseDto(String nickname, String email, String introduction) {
+    public GetMyInfoResponseDto(String nickname, String email, String introduction){
         this.nickname = nickname;
         this.email = email;
         this.introduction = introduction;
