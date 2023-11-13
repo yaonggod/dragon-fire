708e0e022d9f924482917340a989ee0a4b0769cb
김재석 <jaeseki@gmail.com>
2023-11-13 10:57:36 +0900

    :sparkles: Feat: 랭킹페이지 티어 표시 S09P31A209-492

--- frontend/lib/screens/ranking_screen.dart
+++ frontend/lib/screens/ranking_screen.dart
@@ -56,7 +56,7 @@ class RankingScreen extends StatelessWidget {
                               userInfo.nickname,
                               style: const TextStyle(
                                 color: Colors.white,
-                                fontSize: 35,
+                                fontSize: 25,
                               ),
                             ),
                           ),
@@ -146,7 +146,7 @@ class RankingScreen extends StatelessWidget {
                                 userInfo.nickname,
                                 style: const TextStyle(
                                   color: Colors.white,
-                                  fontSize: 25,
+                                  fontSize: 20,
                                 ),
                               ),
                               subtitle: Text(
@@ -156,6 +156,7 @@ class RankingScreen extends StatelessWidget {
                                   fontSize: 20,
                                 ),
                               ),
+                              trailing: setTierImg(userInfo),
                             ),
                           );
                         },
@@ -194,7 +195,7 @@ class RankingScreen extends StatelessWidget {
               },
             ),
           ),
-          Flexible(
+          const Flexible(
             flex: 0,
             child: Padding(
               padding: EdgeInsets.only(bottom: 10, top: 0),
@@ -209,20 +210,82 @@ class RankingScreen extends StatelessWidget {
     );
   }
 
+  Widget setTierImg(TotalRankingModel userInfo) {
+    int score = int.parse(userInfo.score);
+    if (score < 1000) {
+      return SizedBox(
+        width: 32,
+        height: 32,
+        child: Image.asset(
+          'lib/assets/icons/tierBronze.png',
+          fit: BoxFit.cover,
+        ),
+      );
+    } else if (1000 <= score && score < 1100) {
+      return SizedBox(
+        width: 32,
+        height: 32,
+        child: Image.asset(
+          'lib/assets/icons/tierSilver.png',
+          fit: BoxFit.cover,
+        ),
+      );
+    } else if (1100 <= score && score < 1200) {
+      return SizedBox(
+        width: 32,
+        height: 32,
+        child: Image.asset(
+          'lib/assets/icons/tierGold.png',
+          fit: BoxFit.cover,
+        ),
+      );
+    } else if (1200 <= score && score < 1300) {
+      return SizedBox(
+        width: 32,
+        height: 32,
+        child: Image.asset(
+          'lib/assets/icons/tierPlatinum.png',
+          fit: BoxFit.cover,
+        ),
+      );
+    } else {
+      return SizedBox(
+        width: 32,
+        height: 32,
+        child: Image.asset(
+          'lib/assets/icons/tierDiamond.png',
+          fit: BoxFit.cover,
+        ),
+      );
+    }
+  }
+
   Widget topRanking(TotalRankingModel userInfo) {
     if (userInfo.rank == '1') {
-      return Image.asset('lib/assets/icons/rankFirst.png');
+      return Image.asset(
+        'lib/assets/icons/rankFirst.png',
+        width: 40,
+        height: 40,
+      );
     } else if (userInfo.rank == '2') {
-      return Image.asset('lib/assets/icons/rankSecond.png');
+      return Image.asset(
+        'lib/assets/icons/rankSecond.png',
+        width: 40,
+        height: 40,
+      );
     } else if (userInfo.rank == '3') {
-      return Image.asset('lib/assets/icons/rankThird.png');
+      return Image.asset(
+        'lib/assets/icons/rankThird.png',
+        width: 40,
+        height: 40,
+      );
     }
     return Text(
-      '${userInfo.rank}등',
+      userInfo.rank,
       style: const TextStyle(
-        fontSize: 30,
+        fontSize: 20,
         fontWeight: FontWeight.bold,
-        color: Color.fromRGBO(238, 58, 58, 1),
+        color: Colors.white,
         shadows: [
           Shadow(
             offset: Offset(0.5, 0.5),
