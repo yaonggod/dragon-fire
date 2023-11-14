ab8c090a08c6793007f708b0bfa655d79ea55174
Kim EuiNyun <kimeuinyun1@gmail.com>
2023-11-14 09:24:42 +0900

    :sparkles: Feat: S09P31A209-580 자기 소개 구현 완료
    - 테스트도 완료 했습니다.
    - 수정, 조회 모두 가능합니다.

--- BE/dragong/src/main/java/com/dragong/dragong/domain/member/controller/IntroduceController.java
+++ BE/dragong/src/main/java/com/dragong/dragong/domain/member/controller/IntroduceController.java
@@ -4,13 +4,15 @@ import com.dragong.dragong.domain.member.dto.request.UpdateMyIntroduceRequestDto
 import com.dragong.dragong.domain.member.service.IntroduceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/resolve")
 public class IntroduceController {
 
@@ -20,14 +22,16 @@ public class IntroduceController {
     @GetMapping
     public ResponseEntity<?> getMyIntroduce(@RequestHeader("Authorization") String accessToken) {
         try {
-            return new ResponseEntity<>(introduceService.getMyIntroduce(accessToken), HttpStatus.OK);
+            return new ResponseEntity<>(introduceService.getMyIntroduce(accessToken),
+                HttpStatus.OK);
         } catch (NullPointerException NPE) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
     @PatchMapping
-    public ResponseEntity<?> updateMyIntroduce(@RequestHeader("Authorization") String accessToken, UpdateMyIntroduceRequestDto updateMyIntroduceRequestDto) {
+    public ResponseEntity<?> updateMyIntroduce(@RequestHeader("Authorization") String accessToken,
+        @RequestBody UpdateMyIntroduceRequestDto updateMyIntroduceRequestDto) {
         try {
             introduceService.updateMyIntroduce(accessToken, updateMyIntroduceRequestDto);
             return new ResponseEntity<>(HttpStatus.OK);
